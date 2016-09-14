db5271ac6af4c4c32d1a202ff2b9a365d6426dac
aacohen <arielaades@gmail.com>
2016-09-14 10:15:44 -0400

    Functions read me playground

---
+++ swiftFunctionsPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func sayHello () {
+    let greeting = "Hello there!"
+    print (greeting)
+}
+
+sayHello()
+sayHello()
+
+func sayHelloToGarfield() {
+    print("Hello, Garfield, why do you sleep so much?")
+}
+
+func sayHelloToBuster() {
+    print("Hello, Buster, why do you sleep so much?")
+}
+
+func sayHelloToScratchy() {
+    print("Hello, Scratchy, why do you sleep so much?")
+}
+
+func sayHelloToCat (catName: String) {
+    print("Hello, \(catName), why do you sleep so much?")
+}
+
+sayHelloToCat("Mittens")
+
+let catName = "Socks"
+sayHelloToCat(catName)
